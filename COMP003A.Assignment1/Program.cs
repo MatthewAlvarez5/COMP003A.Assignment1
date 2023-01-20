bebd60c70a390aafb116f575681785e0d81fca35
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-01-19 17:02:46 -0800

    commit

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -1,10 +1,119 @@
-﻿namespace COMP003A.Assignment1
+﻿/*
+ * Author: Matthew Alvarez
+ * Course: COMP003A
+ * Purpose: Assignment 1 Familiarize with the program.
+ */
+
+
+
+namespace COMP003A.Assignment1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\t100 Things About Me");
+            Console.WriteLine("-----------------------------------\n");
+            Console.WriteLine("1. My full name is Matthew Isaiah Alvarez");
+            Console.WriteLine("2. My major is Computer Science");
+            Console.WriteLine("3. I change my major multiple times");
+            Console.WriteLine("4. I look forward to participating in this course");
+            Console.WriteLine("5. 5 is my favorite number");
+            Console.WriteLine("6. My favorite color is blue");
+            Console.WriteLine("7. My favorite food is bbq pork ribs");
+            Console.WriteLine("8. This is my 3rd year at West Hills Lemoore");
+            Console.WriteLine("9. I am Currently 20 years old");
+            Console.WriteLine("10. My Birthday is February 8th");
+            Console.WriteLine("11. I have 2 sisters");
+            Console.WriteLine("12. I am a middle-child");
+            Console.WriteLine("13. Both of my sisters are about 2 years seperated from me");
+            Console.WriteLine("14. My Parents are both of Mexican descent");
+            Console.WriteLine("15. I have Graduated with 4 AST degrees from West Hills:");
+            Console.WriteLine("16. \tI earned an AST for Engineering");
+            Console.WriteLine("17. \tI earned an AST for Physics");
+            Console.WriteLine("18. \tI earned an AST for Mathematics");
+            Console.WriteLine("19. \tI earned an AST for Liberal Arts: Math and Science");
+            Console.WriteLine("20. My favorite sport is American Football");
+            Console.WriteLine("21. I am a fan of LA sports teams");
+            Console.WriteLine("22. \tGo Dodgers");
+            Console.WriteLine("23. \tGo Rams");
+            Console.WriteLine("24. \tGo Lakers");
+            Console.WriteLine("25. I like to read");
+            Console.WriteLine("26. I've read 1 and a half books in A Song of Ice and Fire");
+            Console.WriteLine("27. My favorite TV show is the first half of Game of Thrones");
+            Console.WriteLine("28. I like to play video games:");
+            Console.WriteLine("29. \tCall of Duty");
+            Console.WriteLine("30. \tHalo");
+            Console.WriteLine("31. \tSkyrim");
+            Console.WriteLine("32. \tOverwatch");
+            Console.WriteLine("33. \tGTA");
+            Console.WriteLine("34. \tRainbow 6 Siege");
+            Console.WriteLine("35. I work at West Hills Lemoore");
+            Console.WriteLine("36. I work in the WIN Center on Campus");
+            Console.WriteLine("37. This is a long list");
+            Console.WriteLine("38. I'm running out of things to say");
+            Console.WriteLine("39. I like comedy");
+            Console.WriteLine("40. My favorite comedian is Conan O'Brien");
+            Console.WriteLine("41. I like to watch Youtube");
+            Console.WriteLine("42. I think Mark Rober is great at engineering and entertaining on Youtube");
+            Console.WriteLine("43. I like dogs");
+            Console.WriteLine("44. I have 2 dogs");
+            Console.WriteLine("45. I have a boxer named Dexter");
+            Console.WriteLine("46. I have a Rhodesian Ridgeback named Layla");
+            Console.WriteLine("47. Dexter is about 1 year");
+            Console.WriteLine("48. Layla is about 6 years");
+            Console.WriteLine("49. I've never owned a cat");
+            Console.WriteLine("50. Wow only half way?");
+            Console.WriteLine("51. I own a 2017 Toyota Corolla");
+            Console.WriteLine("52. I've only put about 6,000 miles on it");
+            Console.WriteLine("53. I got it a mont or so prior to the COVID-19 outbreak");
+            Console.WriteLine("54. I like to assemble Keyboards");
+            Console.WriteLine("55. But they're expensive");
+            Console.WriteLine("56. I have 3 keyboards");
+            Console.WriteLine("57. \tOne has linear switches I use for games");
+            Console.WriteLine("58. \tOne has tactile switches I use for typing and a volume nob");
+            Console.WriteLine("59. \tOne has linear switches as well, but I dont know whether to change them or not");
+            Console.WriteLine("60. \tAnd they were all too expensive");
+            Console.WriteLine("61. I love to listen to music");
+            Console.WriteLine("62. My favorite Genre is classic rock");
+            Console.WriteLine("63. I like to listen to other genres though");
+            Console.WriteLine("64. My favorite bands are all different yet similar genres:");
+            Console.WriteLine("65. \t1.The Eagles");
+            Console.WriteLine("66. \t2.Led Zeppelin");
+            Console.WriteLine("67. \t3.Metallica");
+            Console.WriteLine("68. \t4.Red Hot Chili Peppers");
+            Console.WriteLine("69. Heh, Nice.");
+            Console.WriteLine("70. Almost done");
+            Console.WriteLine("71. I like to listen to soundtracks as well");
+            Console.WriteLine("72. I think that most soundtracks take inspiration from classical music");
+            Console.WriteLine("73. So, I like to listen to soundtracks while I study");
+            Console.WriteLine("74. My favorite music composer is John Williams");
+            Console.WriteLine("75. The Star Wars films are some of my favorite movies");
+            Console.WriteLine("76. My Favorite Movies are Typically older");
+            Console.WriteLine("77. \t1.Pulp Fiction");
+            Console.WriteLine("78. \t2.The Godfather Part II");
+            Console.WriteLine("79. \t3.Gladiator");
+            Console.WriteLine("80. \t4.Good Will Hunting");
+            Console.WriteLine("81. \t5.Star Wars: Episode III - Revenge of the Sith");
+            Console.WriteLine("82. My favorite letter is B");
+            Console.WriteLine("83. My favorite computer mice are Logitech");
+            Console.WriteLine("84. The mouse I use daily is the Logitech G604");
+            Console.WriteLine("85. I use an iPhone");
+            Console.WriteLine("86. My first phone was a slide phone");
+            Console.WriteLine("87. I've had my iPhone for over 4 years");
+            Console.WriteLine("88. I want a Galaxy Fold");
+            Console.WriteLine("89. My favorite brand of clothing is Carhartt");
+            Console.WriteLine("90. I like Levi jeans though");
+            Console.WriteLine("91. I wear glasses");
+            Console.WriteLine("92. I don't like contacts");
+            Console.WriteLine("93. I've only learned C and C++ coding before");
+            Console.WriteLine("94. I attended Lemoore High");
+            Console.WriteLine("95. I was enrolled in the Engineering Pathway");
+            Console.WriteLine("96. I learned to use VEX Robotics which was coded in C++");
+            Console.WriteLine("97. I learned to 3D Model in the course as well");
+            Console.WriteLine("98. I've learned how to model in Solid Works and 3D CAD");
+            Console.WriteLine("99. I'm an Aquarius");
+            Console.WriteLine("100. Finally, I'm glad this is over");
         }
     }
 }
